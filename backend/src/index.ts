ec2ab31e648b33e6fd32d78c2e8df2e45fb4a38a
name goes here <fake@email.com>
2024-06-18 18:03:16 +0100

    Initialised backend project

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+// src/index.ts
+import express, {Express} from 'express';
+import dotenv from 'dotenv';
+import PostController from './controllers/posts'
+
+dotenv.config();
+
+const app: Express = express();
+const port = 3000;
+
+app.get('/posts', PostController)
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
